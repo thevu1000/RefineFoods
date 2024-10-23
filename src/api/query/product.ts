c629cbb3eee008dc0e89681c0a7e16b6c0a5ce05
NguyenTheVu551 <vu.nt@dktech.info>
2024-10-23 09:18:12 +0700

    keepPreviousData Paginate

--- src/api/query/product.ts
+++ src/api/query/product.ts
@@ -1,10 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery , keepPreviousData} from '@tanstack/react-query';
 import { fetchProducts } from '@/api/fetchData';
 
 export const useFetchProducts = (currentPage: number, start: number, end: number, categoryId: number) => {
-    const { data: { data: allProducts, totalCount } = {}, error, isLoading } = useQuery({
+    const { data: { data: allProducts, totalCount} = {}, error, isLoading, isPlaceholderData } = useQuery({
         queryKey: ['products', categoryId, currentPage],
         queryFn: () => fetchProducts(start, end, categoryId),
+        placeholderData: keepPreviousData,
         refetchOnMount: false,
         refetchOnWindowFocus: false,
         staleTime: 1000 * 60 * 5, // Dữ liệu được coi là mới trong 5 phút
